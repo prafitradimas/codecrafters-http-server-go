0fa6b632e87bcb8a158b1a5fc610f63a5e3352a8
Prafitra Dimas <prftrdms@gmail.com>
2024-05-19 00:42:34 +0700

    pass Read header

--- app/server.go
+++ app/server.go
@@ -8,6 +8,15 @@ import (
 )
 
 type (
+	Request struct {
+		httpversion string
+		host        string
+		method      string
+		path        string
+		headers     map[string]string
+		body        string
+	}
+
 	Response struct {
 		httpversion string
 		code        int
@@ -34,45 +43,33 @@ func main() {
 
 	buf := make([]byte, 4096)
 	conn.Read(buf)
+	fmt.Println(string(buf))
 
-	_, path, proto, _ := parseRequest(string(buf))
-	_, body := separateHeadersAndBody(proto)
+	req, _ := parseRequest(string(buf))
 	res := &Response{
 		httpversion: "1.1",
-		body:        body,
+		body:        req.body,
 	}
 
-	param, found := strings.CutPrefix(path, "/echo/")
-	if path == "/" {
+	param, found := strings.CutPrefix(req.path, "/echo/")
+	if req.path == "/" {
 		res.code = 200
+		write(conn, res)
+	} else if req.path == "/user-agent" {
+		res.code = 200
+		res.body = req.headers["User-Agent"]
+		res.headers = []string{"Content-Type: text/plain", fmt.Sprintf("Content-Length: %d", len(res.body))}
 		write(conn, res)
 	} else if found {
 		res.code = 200
 		res.body = param
-		res.headers = []string{"Content-Type: text/plain", fmt.Sprintf("Content-Length: %d", len(param))}
+		res.headers = []string{"Content-Type: text/plain", fmt.Sprintf("Content-Length: %d", len(res.body))}
 		write(conn, res)
 	} else {
 		res.code = 404
 		write(conn, res)
 	}
 
-}
-
-func parseRequest(s string) (method, path, proto string, ok bool) {
-	method, aftercut1, ok1 := strings.Cut(s, " ")
-	path, proto, ok2 := strings.Cut(aftercut1, " ")
-
-	if !ok1 || !ok2 {
-		return "", "", "", false
-	}
-
-	return method, path, proto, true
-}
-
-func separateHeadersAndBody(s string) (rawheaders, rawbody string) {
-	split := strings.Split(s, "\r\n\r\n")
-
-	return split[0], split[1]
 }
 
 func write(conn net.Conn, res *Response) {
@@ -90,6 +87,39 @@ func write(conn net.Conn, res *Response) {
 	}
 }
 
+func parseRequest(s string) (req *Request, ok bool) {
+	method, after1, ok1 := strings.Cut(s, " ")
+	path, after2, ok2 := strings.Cut(after1, " ")
+	httpversion, after3, ok3 := strings.Cut(after2, "\r\n")
+	fmt.Println("after3:", after3)
+
+	if !ok1 || !ok2 || !ok3 {
+		return nil, false
+	}
+
+	rawheaders, rawbody := separateHeadersAndBody(after3)
+
+	req = &Request{}
+	req.httpversion = httpversion
+	req.method = method
+	req.path = path
+	req.headers = mapheaders(strings.Split(rawheaders, "\r\n"))
+	req.body = rawbody
+
+	fmt.Printf("httpversion: %s\n", req.httpversion)
+	fmt.Printf("method: %s\n", req.method)
+	fmt.Printf("path: %s\n", req.path)
+	fmt.Printf("headers: %v\n", req.headers)
+	fmt.Printf("body: %v\n", req.body)
+	return req, true
+}
+
+func separateHeadersAndBody(s string) (rawheaders, rawbody string) {
+	split := strings.Split(s, "\r\n\r\n")
+
+	return split[0], split[1]
+}
+
 func httpstatus(code int) string {
 	switch code {
 	case 200:
@@ -99,4 +129,19 @@ func httpstatus(code int) string {
 	default:
 		return "OK"
 	}
+}
+
+func mapheaders(ss []string) map[string]string {
+	headers := make(map[string]string)
+	fmt.Println(ss)
+	for _, s := range ss {
+		split := strings.Split(s, ": ")
+		if 2 != len(split) {
+			panic("ERROR: Malformed header")
+		}
+
+		headers[split[0]] = split[1]
+	}
+
+	return headers
 }
